34f2a6d004e5835d74a452c00a1d52afd094677b
ddyy <1@q.c>
2020-06-18 16:56:43 +0800

    update

--- src/main/java/com/dy/Util/ConvertUtil.java
+++ src/main/java/com/dy/Util/ConvertUtil.java
@@ -5,7 +5,7 @@ import java.io.UnsupportedEncodingException;
 public class ConvertUtil {
 	
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		String a = "public.\"CRS\"";
+		String a = "";
 		String cover = convertAll(a.getBytes("UTF-8"));
 		System.out.println(cover);
 		System.out.println(new String(unConvertAll(cover),"UTF-8"));

ba7e11b19775309b74581faf42b15a7d0a255021
ddyy <1@q.c>
2020-06-27 16:02:29 +0800

    update

--- src/main/java/com/dy/Util/ConvertUtil.java
+++ src/main/java/com/dy/Util/ConvertUtil.java
@@ -5,7 +5,7 @@ import java.io.UnsupportedEncodingException;
 public class ConvertUtil {
 	
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		String a = "";
+		String a = "jdbc:postgresql://10.2.2.48:5432/typhoon";
 		String cover = convertAll(a.getBytes("UTF-8"));
 		System.out.println(cover);
 		System.out.println(new String(unConvertAll(cover),"UTF-8"));

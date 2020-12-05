8bce21af2bd908196cd6c6e8c91192393e782feb
YD <YD@DYJA.com>
2020-12-05 21:05:09 +0800

    添加初步设计文档,明日开始开发。

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+    cache: new InMemoryCache()
+});
+
 ReactDOM.render(<div>test 123</div>, document.getElementById("root"))
